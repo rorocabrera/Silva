2eb84f5da8fb261cc8b381ee17ddb809815c253b
rorocabrera <rorocabrera@gmail.com>
2022-06-08 16:10:33 -0400

    commits

--- silva_admin/lib/Widgets/MyAppbar/app_bar.dart
+++ silva_admin/lib/Widgets/MyAppbar/app_bar.dart
@@ -3,5 +3,31 @@ import 'package:get/get.dart';
 import 'package:silva_admin/controllers/main_controller.dart';
 
 PreferredSizeWidget? MyAppBar(String name) {
-  return AppBar();
+  return AppBar(
+    actions: [
+      Obx(() => _connection()),
+    ],
+  );
+}
+
+Widget _connection() {
+  MainController mainCnt = Get.find();
+  Icon icon;
+  switch (mainCnt.serverAct.value) {
+    case 0:
+      icon = Icon(Icons.wifi_off);
+      break;
+    case 1:
+      icon = Icon(Icons.wifi);
+      break;
+    case 2:
+      icon = Icon(Icons.wifi_find);
+
+      break;
+    default:
+      icon = Icon(Icons.wifi_off);
+      break;
+  }
+
+  return Visibility(child: IconButton(onPressed: () {}, icon: icon));
 }
